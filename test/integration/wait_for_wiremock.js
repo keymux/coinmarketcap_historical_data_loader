b71a78de4ed620b984418705df4774fe0300c590
Henry Brown <henry@ibespwn.com>
2018-07-14 01:28:39 -0700

    Wait for wiremock to load before starting integration tests

---
+++ test/integration/wait_for_wiremock.js
@@ -0,0 +1,30 @@
+const rp = require("request-promise");
+
+before(function() {
+  return new Promise((resolve, reject) => {
+    const start = Date.now();
+    const timeout = 5000;
+    // add 5% so the below function will finish first
+    this.timeout = timeout * 0.05;
+
+    const retry = () => {
+      return rp({
+        uri: `http://localhost:${process.env.WIREMOCK_PORT}/__admin/mappings`,
+      })
+        .then(res => {
+          console.error("success");
+
+          resolve(res);
+        })
+        .catch(err => {
+          if (start + timeout < Date.now()) {
+            return reject(err);
+          }
+
+          setTimeout(retry);
+        });
+    };
+
+    setTimeout(retry);
+  });
+});
