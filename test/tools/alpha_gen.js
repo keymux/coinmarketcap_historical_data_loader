db65e780c253fbf892b36820b407be40e8987ca4
Henry Brown <henry@ibespwn.com>
2018-07-29 17:36:08 -0700

    Added testing tools

--- test/tools/alpha_gen.js
+++ test/tools/alpha_gen.js
@@ -1,11 +1,15 @@
 const RandExp = require("randexp");
 const alphaRandExp = new RandExp(/[A-Za-z]{10,30}/);
 
+const alphaGen = alphaRandExp.gen.bind(alphaRandExp);
+
+/*
 const alphaGen = () => {
   const gen = alphaRandExp.gen();
 
   return gen;
 };
+*/
 
 module.exports = {
   alphaGen,
