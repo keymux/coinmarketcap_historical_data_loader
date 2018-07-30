a637cd1003f521e44125400e1ebee2b81f986f89
Henry Brown <henry@ibespwn.com>
2018-07-29 17:27:01 -0700

    Improved generation of mariadb env

--- scripts/generate_mariadb_env.sh
+++ scripts/generate_mariadb_env.sh
@@ -9,6 +9,12 @@ ROOT_DIR="$(realpath "${SCRIPTS_DIR}/..")"
 ENV_IN="${ENV_DIR}/mariadb.env.example"
 ENV_OUT="${ENV_DIR}/mariadb.env"
 
-cat "${ENV_IN}" | sed "s/CHANGEME/$(randomString)/" > ${ENV_OUT}
+cat "${ENV_IN}" | while read line; do
+  while echo $line | grep "CHANGEME" > /dev/null; do
+    line=$(echo "${line}" | sed "s/CHANGEME/$(randomString)/")
+  done
+
+  echo "${line}"
+done > "${ENV_OUT}"
 
 echo "Generated ${ENV_OUT}" >&2
